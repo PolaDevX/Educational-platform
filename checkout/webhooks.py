0269db38a3e59a5dda432da350d5e04433a51798
PolaDevX <pola.pp9291@gmail.com>
2026-09-07 18:03:40 +0300

    init .

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -4,17 +4,13 @@ import stripe
 from django.http import HttpResponse
 from checkout import models
 from courses.models import Order, Course
-from django.template.loader import render_to_string
-from django.core.mail import send_mail
-from paypal.standard.models import ST_PP_COMPLETED
-from paypal.standard.ipn.signals import valid_ipn_received
+import json
 
 @csrf_exempt
 def stripe_webhook(request):
     print('stripe webhook')
     payload = request.body
     sig_header = request.META.get('HTTP_STRIPE_SIGNATURE')
-
 
     try:
         event = stripe.Webhook.construct_event(
@@ -28,10 +24,11 @@ def stripe_webhook(request):
         return HttpResponse(status=400)
 
     if event and event['type'] == 'payment_intent.succeeded':
-        payment_intent = event['data']['object']  # contains a stripe.PaymentIntent
+        payment_intent = event['data']['object']
         print('payment_intent.succeeded')
         print(payment_intent.metadata)
-        transaction_id = payment_intent.metadata['transaction']
+        
+        transaction_id = int(payment_intent.metadata['transaction'])
         make_order(transaction_id)
     else:
         print('Unhandled event type {}'.format(event['type']))
@@ -43,7 +40,6 @@ def make_order(transaction_id):
 
     items = transaction.items
     if isinstance(items, str):
-        import json
         try:
             items = json.loads(items)
         except:
@@ -68,20 +64,4 @@ def make_order(transaction_id):
         order.orderproduct_set.create(
             course_id=course.id,
             price=course.price
-        )
-
-    customer_email = transaction.customer.get('email') if isinstance(transaction.customer, dict) else getattr(transaction, 'customer_email', None)
-    
-    if customer_email:
-        msg_html = render_to_string('emails/order.html', {
-            'order': order,
-            'courses': courses,
-        })
-
-        send_mail(
-            subject='Order Completed',
-            html_message=msg_html,
-            message=msg_html,
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=[customer_email],
         )
